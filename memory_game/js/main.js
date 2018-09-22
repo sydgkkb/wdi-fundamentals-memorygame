2d0daf155d02c4ef0e6471778b898cfd028b505d
Kiran Bandaru <kiran_bandaru@glic.com>
2018-09-22 16:17:24 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,34 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if(cardsInPlay.length === 2){
-
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match");
-	}else{
-		alert("Sorry, try again");
+var checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
 	}
 }
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(1);
+checkForMatch();
+
+/*	
+	if(cardsInPlay.length === 2){
+
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match");
+		}else{
+			alert("Sorry, try again");
+		}
+	}
+	*/
+
+
+
+
 
 
